a62788a0f84537b7d85771ca5f8713f30eb2a9c8
Felipe Apablaza <ing.apablaza@gmail.com>
2015-02-15 23:22:26 -0300

    Fixed empty demo application.

--- src/exeng.demo/Demo01.cpp
+++ src/exeng.demo/Demo01.cpp
@@ -2,16 +2,49 @@
 #include <boost/filesystem/path.hpp>
 #include <exeng/framework/GraphicsApplication.hpp>
 #include <exeng/system/PluginManager.hpp>
+#include <exeng/graphics/GraphicsManager.hpp>
+#include <exeng/graphics/GraphicsDriver.hpp>
 
 using namespace exeng;
 using namespace exeng::framework;
+using namespace exeng::graphics;
+using namespace exeng::scenegraph;
 
 class Demo01 : public GraphicsApplication {
 public:
     virtual void initialize(int argc, char **argv) override 
     {
-        this->getRoot()->getPluginManager()->loadPlugins();
+        this->getRoot()->getPluginManager()->setPluginPath("../../plugins/libexeng.graphics.gl3/");
+        this->getRoot()->getPluginManager()->loadPlugin("exeng.graphics.gl3");
+        
+        this->graphicsDriver = std::move(this->getRoot()->getGraphicsManager()->createDriver());
+        this->graphicsDriver->initialize();
     }
+    
+    virtual ApplicationStatus::Enum getStatus() const override 
+    {
+        return ApplicationStatus::Running;
+    }
+    
+    virtual void pollEvents() override 
+    {
+        
+    }
+    
+    virtual void update(double seconds) override 
+    {
+        
+    }
+    
+    virtual void render() override 
+    {
+        this->graphicsDriver->beginFrame({0.0f, 0.2f, 0.8f, 1.0f}, ClearFlags::Color);
+        
+        this->graphicsDriver->endFrame();
+    }
+    
+private:
+    std::unique_ptr<GraphicsDriver> graphicsDriver;
 };
 
 int main(int argc, char **argv) 
