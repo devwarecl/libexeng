67703609a35ee2148074b3be5f013f3c892a2955
soultaker <ing.apablaza@gmail.com>
2015-02-21 15:09:59 -0300

    Added exeng::Exception class.

---
+++ src/exeng/Exception.hpp
@@ -0,0 +1,22 @@
+
+#ifndef __EXENG_EXCEPTION_HPP__
+#define __EXENG_EXCEPTION_HPP__
+
+#include <string>
+#include <stdexcept>
+
+namespace exeng {
+	/**
+	 * @brief Root exception.
+	 */
+	class Exception : public std::runtime_error {
+	public:
+		Exception(const std::string &msg);
+		Exception(const std::string &msg, const std::string &fileName, int line);
+	};
+}
+
+#define EXENG_THROW_EXCEPTION(Message) throw exeng::Exception(Message, __FILE__, __LINE__);
+#define EXENG_THROW(Message, ExceptionClass) throw ExceptionClass(Message, __FILE__, __LINE__);
+
+#endif // __EXENG_EXCEPTION_HPP__
