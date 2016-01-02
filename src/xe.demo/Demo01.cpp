a56f7907007fed7c21fb157c8fd424ff97b855ca
Felipe Apablaza <fapablazacl@protonmail.com>
2016-01-02 20:11:16 -0300

    [+] Added event handling to the demo application

--- src/xe.demo/Demo01.cpp
+++ src/xe.demo/Demo01.cpp
@@ -4,14 +4,14 @@
 #include <xe/sys/Plugin.hpp>
 #include <xe/sys/PluginManager.hpp>
 #include <xe/gfx/GraphicsManager.hpp>
+#include <xe/input/IEventHandler.hpp>
 #include <xe.main/Main.hpp>
 
 namespace demo {
-	class DemoApplication : public xe::fw::Application {
+	class DemoApplication : public xe::fw::Application, public xe::input::IEventHandler {
 	public:
 		DemoApplication() {
 			// load available plugins
-
 			this->getPluginManager()->loadPlugins();
 
 			std::cout << "Loaded plugins: " << std::endl;
@@ -27,7 +27,7 @@ namespace demo {
 
 			std::cout << std::endl;
 
-			// display all available drivers
+			// display all available graphics drivers
 			auto driverInfos = this->getGraphicsManager()->getAvailableDrivers();
 
 			if (driverInfos.size() > 0) {
@@ -40,23 +40,44 @@ namespace demo {
 				throw std::runtime_error("No available graphics drivers.");
 			}
 
-			// select the first one
+			// select the first graphics driver
 			auto graphicsDriver = this->getGraphicsManager()->createDriver(driverInfos[0]);
 
 			this->graphicsDriver = std::move(graphicsDriver);
+
+			this->graphicsDriver->addEventHandler(this);
 		}
 
         virtual int run(int argc, char **argv) override {
 			// initialize the graphics driver with default settings
-			graphicsDriver->initialize();
+			this->graphicsDriver->initialize();
 
-			graphicsDriver->terminate();
+			while (!done) {
+				this->graphicsDriver->pollEvents();
+
+				this->graphicsDriver->beginFrame({0.2f, 0.2f, 0.8f, 1.0f}, xe::gfx::ClearFlags::ColorDepth);
+
+				this->graphicsDriver->endFrame();
+			}
+
+			this->graphicsDriver->terminate();
 
 			return 0;
 		}
 
+		virtual void handleEvent(const xe::input::EventData &eventData) override {
+			if (eventData.eventType == xe::TypeId<xe::gfx::InputEventData>()) {
+				this->done = true;
+
+			} else if (eventData.eventType == xe::TypeId<xe::gfx::CloseEventData>()) {
+				this->done = true;
+			}
+		}
+
 	private:
 		xe::gfx::GraphicsDriverPtr graphicsDriver;
+
+		bool done = false;
 	};
 }
 
